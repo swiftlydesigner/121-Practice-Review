76f01a4daf567a0cfae1e3b956f0609dc4e8b837
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2023-10-19 15:07:25 -0700

    Update Arrays.h
    Add 2D and 3D tasks.

--- Code/headers/Arrays.h
+++ Code/headers/Arrays.h
@@ -60,4 +60,45 @@ inline findMaxArrayReturn * findMaxArray(int * myArr, size_t size) {
 	return returnVal;
 }
 
+
+inline long * sum2DArray(int ** twoDArray, size_t * dims) {
+	/// A 2D array will consist of row A and column B. Return an array S of length len(row A).
+	/// Each element in S should be the sum of the respective B column (that is, start at 0th position in A
+	/// then sum all of B and that is S's 0th value; continue until you have gone over the entire array.)
+	/// dims[0] = A's length, dims[1] = B's length. Use malloc as needed.
+	
+	return NULL;
+}
+
+
+inline long sum2DFlattenArray(int ** twoDArray, size_t * dims) {
+	/// Similar to `sum2DArray`, but you need to sum the array S to produce a long L rather than an array S.
+	/// Use malloc as needed.
+	/// If you need a hint, use a base64 decoder for the following (Linux/macOS: `echo "<MSG>" | base64 -d`).
+	///
+	/// SElOVDogSW1wbGVtZW50IGBzdW0yREFycmF5YCB0aGVuIGNhbGwgdGhhdCBhbmQgc3VtIHRoZSAx
+	/// RCBhcnJheS4K
+	
+	return NULL;
+}
+
+inline long ** sum3DArray(int *** threeDArray, size_t * dims) {
+	/// `threeDArray` will consist of A (array of arrays), B (array of arrays), and C (array of ints). A consists of
+	/// B which consists of C. dims[0] = len(A). dims[1] = len(B). dims[2] = len(C).
+	/// A's final value will consist of an array of arrays B's Final values will be an array of ints.
+	/// Use malloc as needed.
+	
+	return NULL;
+}
+
+inline long sum3DFlattenArray(int *** threeDArray, size_t * dims) {
+	/// Similar to `sum3DArray`, but return the sum of the arrays. Use malloc as needed.
+	/// If you need a hint, use a base64 decoder for the following (Linux/macOS: `echo "<MSG>" | base64 -d`).
+	///
+	/// SElOVDogSW1wbGVtZW50IGBzdW0yREFycmF5YCBhbmQgYHN1bTNEQXJyYXlgIHRoZW4gY2FsbCB0
+	/// aGF0IGFuZCBzdW0gdGhlIDFEIGFycmF5Lgo=
+	
+	return NULL;
+}
+
 #endif /* Arrays_h */
